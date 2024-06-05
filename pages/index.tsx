4e520388ddaae325e02f4210b7a084473e45a51e
Kinpoe.Ray <149134281+kinpoe-ray@users.noreply.github.com>
2024-06-06 00:10:29 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               Kinpoe.Ray's Phoalbum Wall
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Product Manager，Designer.Focus on Product & AI
+              Product Manager,Designer.         Focus on Product & AI.
             </p>
             <a
               className="cursor-pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
